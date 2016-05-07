2f8904095b1f4fde9fefc4c34083aea64b4ff7e8
Uchenna Chilaka <uche.chilaka@gmail.com>
2016-05-06 23:57:28 -0400

    Fixes to OAuth\Server via LarCityOAuthServer

---
+++ dist/application/third_party/larcity/auth/LarCityOAuthServer.php
@@ -0,0 +1,17 @@
+<?php
+
+class LarCityOAuthServer extends OAuth2\Server {
+
+    public function addGrantType(/* \OAuth2\GrantType\GrantTypeInterface */ $grantType, $identifier = null) {
+        //parent::addGrantType($grantType, $identifier);
+        if (!is_string($identifier)) {
+            $identifier = $grantType->getQuerystringIdentifier();
+        }
+        $this->grantTypes[$identifier] = $grantType;
+        // persist added grant type down to TokenController
+        if (!is_null($this->tokenController)) {
+            $this->getTokenController()->addGrantType($grantType, $identifier);
+        }
+    }
+
+}
